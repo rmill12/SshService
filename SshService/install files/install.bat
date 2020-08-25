6ccdd7813901e3fa33de0c510120f3f1fc01e067
Ryan Milligan <1128683+rmill12@users.noreply.github.com>
2020-08-25 14:39:43 -0600

    Update

--- SshService/install files/install.bat
+++ SshService/install files/install.bat
@@ -1,1 +1,1 @@
-"C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe" "%~dp0\..\ServerMonitor.exe"
+sc create "SSH Service" binpath="%~dp0\..\SshService.exe"
--- SshService/install files/uninstall.bat
+++ SshService/install files/uninstall.bat
@@ -1,1 +1,1 @@
-"C:\Windows\Microsoft.NET\Framework\v4.0.30319\InstallUtil.exe" "%~dp0\..\ServerMonitor.exe" -u
+sc delete "SSH Service"
